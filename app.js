59e9bed4d1f0376106cd36e1b500611ff83e5ba5
Connor Alexander <connor.alexander@my.uwrf.edu>
2022-05-12 20:10:18 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Search Input
+const searchUser = document.getElementById('searchUser');
+
+// Search Input Event Listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
+
+  // input validation
+});
