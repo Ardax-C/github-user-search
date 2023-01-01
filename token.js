da919e0e05add78afac4d7688a753f52484e3894
Connor Alexander <104035940+Ardax-C@users.noreply.github.com>
2023-01-01 11:06:42 -0600

    Update token.js

--- token.js
+++ token.js
@@ -1,6 +1,1 @@
-class Token {
-  const githubToken = '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';
-  function getToken() {
-    return token;
-}
-
+const githubToken = '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';
